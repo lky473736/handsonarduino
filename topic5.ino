22efe903002bf2c55fcf52b7433da1c1616d3397
임규연 <lky473736@icloud.com>
2023-04-06 19:46:24 +0900

    Create topic5.ino

---
+++ topic5.ino
@@ -0,0 +1,22 @@
+int melody[] = {262,293,329,349,392};
+void setup()
+{
+  pinMode(10, OUTPUT);
+  for (int buttonpin = 2; buttonpin < 7; buttonpin++)
+  {
+    pinMode(buttonpin, INPUT);
+  }
+}
+
+void loop()
+{
+  for (int i = 2; i < 7; i++)
+  {
+    if (digitalRead(i) == HIGH)
+    {
+      tone(10, melody[i-2], 250);
+      delay(500);
+    }
+  }
+  noTone(10);
+}

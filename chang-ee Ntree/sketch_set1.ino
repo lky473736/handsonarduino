4c332e284d512579ac4183b1ad1b740fe9a55c04
임규연 <lky473736@icloud.com>
2023-05-04 23:02:02 +0900

    Update sketch_set1.ino

--- chang-ee Ntree/sketch_set1.ino
+++ chang-ee Ntree/sketch_set1.ino
@@ -1,10 +1,11 @@
-#include <LiquidCrystal.h>
+#include <LiquidCrystal_I2C.h>
 #include <Wire.h>
 #include <Servo.h>
 #include <LedControl.h>
 
-LedControl dot = LedControl(A2, A3); // Dot matrix 핀번호 입력
-// 작동이 이상하다 - 디지털 핀에 연결
+LedControl Dote = LedControl(7, 6, 5, 1);
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 byte triangle[]={
   B00000000,
@@ -31,18 +32,20 @@ byte stick[]={
 int trig = 3;
 int echo = 2;
 
-Servo servo1;
-Servo servo2; 
-Servo servo3; 
-Servo servo4; 
+char input1;
+
+int a, b;
+
+Servo myservo1;  // 서보 모터 1 객체
+Servo myservo2;  // 서보 모터 2 객체
+int servo1 = 4;
+int servo2 = 5;
 
 int red = 8;
 int green = 9;
 
 int sda_d = A2; // dot matrix
 int scl_d = A3; // dot matrix
-
-LiquidCrystal lcd(A4, A5); // 디지털 핀으로 고쳐야 할 수도 있음
 
 long duration;
 int distance;
@@ -61,53 +64,50 @@ void setup() {
   Serial.begin(9600);
   pinMode(trig, OUTPUT);
   pinMode(echo, INPUT);
-  servo1.attach(4);
-  servo2.attach(5);
-  servo3.attach(6);
-  servo4.attach(7);
   pinMode(red, OUTPUT);
   pinMode(green, OUTPUT);
 
-  dot.shutdown(0, false);
-  dot.setIntensity(0, 8);
-  dot.clearDisplay(0);
-}
+  Dote.shutdown(0, false);                    
+  Dote.setIntensity(0, 7);                 
+  Dote.clearDisplay(0);
 
-void loop() {
   Serial.print("Input maths symbols : ");
 
-  if (Serial.available() > 0) {
-    char input1 = Serial.read();
-    if (input1 == '+' || input1 == '-' || input1 == '*' || input1 == '/') {
-      int a, b;
-      if (Serial.available() >= 2) {
-        a = Serial.parseInt();
-        b = Serial.parseInt(); // 여기 틀릴수도 있음
-      }
 
-      choumpa();
-      if (distance < 100) {
-        delay (15000);
 
-        if (distance < 100) {
-          lcd.begin(16, 2);
-          int result = ac_calculator(a, b, input1);
-          lcd.print(result);
-          digitalWrite(green, HIGH);
-          delay(10000);
-          digitalWrite(green, Low);
-        }
-
-        else {
-          fa_calculator();
-          digitalWrite(red, HIGH);
-          delay(10000);
-          digitalWrite(red, Low);
-        }
-      }
-    }
+  input1 = Serial.read();
+  if (input1 == '+' || input1 == '-' || input1 == '*' || input1 == '/') {
+    if (Serial.available() >= 2) {
+      a = Serial.parseInt();
+      b = Serial.parseInt(); 
+    }                          
   }
 }
+
+void loop(){
+  choumpa();
+    if (distance < 100) {
+      delay (15000);
+
+      if (distance < 100) {
+        lcd.begin(16, 2);
+        int result = ac_calculator(a, b, input1);
+        lcd.print(result);
+        digitalWrite(green, HIGH);
+        delay(10000);
+        digitalWrite(green, LOW);
+      }
+
+      else {
+        fa_calculator();
+        digitalWrite(red, HIGH);
+        delay(10000);
+        digitalWrite(red, LOW);
+      }
+    }
+}
+
+
 
 void notattention1(int time) // time 만큼 잠자기
 {  
@@ -117,9 +117,7 @@ void notattention1(int time) // time 만큼 잠자기
 
 void notattention2(int time) // time 만큼 춤추기
 {  
-  Servo myservo1;
   myservo1.attach(servo1);
-  Servo myservo2;
   myservo2.attach(servo2);
 
   for (int pos = 150; pos <= 210; pos += 1) {  // 150도부터 210도까지 회전
@@ -134,8 +132,6 @@ void notattention2(int time) // time 만큼 춤추기
     delay(15);  // 15밀리초 딜레이
     dotmatrix_tri();
   }
-  myservo1.detach();
-  myservo2.detach();
 }
 
 void choumpa()
@@ -181,7 +177,7 @@ int fa_calculator()
 void dotmatrix_tri()
 {
   for (int i = 0; i < 8; i++) {
-    dot.setRow(0, i, triangle[i]);
+    Dote.setRow(0, i, triangle[i]);
     delay(150);  // 150밀리초 딜레이
   }
 }
@@ -189,7 +185,7 @@ void dotmatrix_tri()
 void dotmatrix_sti()
 {
   for (int i = 0; i < 8; i++) {
-    dot.setRow(0, i, stick[i]);
+    Dote.setRow(0, i, stick[i]);
     delay(150);  // 150밀리초 딜레이
   }
 }
